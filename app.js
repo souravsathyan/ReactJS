606766cd659dc8299171506000c09f17859b203e
souravsathyan <souravksathyan@gmail.com>
2023-12-27 20:39:57 +0530

    episode 1

---
+++ app.js
@@ -0,0 +1,12 @@
+const parent = React.createElement(
+    "div", { id: 'parent' },
+    [React.createElement('div', { id: 'child1' },
+        [React.createElement('h1', {}, "I'm a H1 Tag in nested div"), React.createElement('h2', {}, "I'm a H2 Tag in nested div")]
+    ), React.createElement('div', { id: 'child2' },
+        [React.createElement('h1', {}, "I'm a H1 Tag in nested div"), React.createElement('h2', {}, "I'm a H2 Tag in nested div")]
+    )]
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(parent)
